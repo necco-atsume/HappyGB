d97f3229b33d9590eb74e26696b3df9e877f0fea
Corey Eckelman <corick2@gmail.com>
2014-01-07 22:46:18 -0800

    Fixed attempting to read cart RAM in a ROM-only cart type causing an exception.

--- HappyGB/Core/NoMBC.cs
+++ HappyGB/Core/NoMBC.cs
@@ -3,28 +3,29 @@ using System.IO;
 
 namespace HappyGB.Core
 {
-	public class NoMBC
-		: IMemoryBankController
-	{
-		private byte[] rom;
+    public class NoMBC
+        : IMemoryBankController
+    {
+        private byte[] rom;
 
-		public NoMBC(CartReader cart)
-		{
-			rom = cart.GetBuffer(0x8000);
-		}
+        public NoMBC(CartReader cart)
+        {
+            rom = cart.GetBuffer(0x8000);
+        }
 
-		#region IMemoryBankController implementation
+        #region IMemoryBankController implementation
 
-		public void Reset() { /* nothing */ }
+        public void Reset() { /* nothing */ }
 
-		public byte Read8(ushort address)
-		{
-			return rom[address];
-		}
+        public byte Read8(ushort address)
+        {
+            if (address > 0x8000) return 0; //RAM
+            else return rom[address];
+        }
 
-		public void Write8(ushort address, byte value) { /* nothing */ }
+        public void Write8(ushort address, byte value) { /* nothing */ }
 
-		#endregion
-	}
+        #endregion
+    }
 }
 
