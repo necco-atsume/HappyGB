1e744b908a74b714bc68ec6dc7fb7fdac6e1e5d4
Corey Eckelman <corey@coreyeckelman.com>
2014-07-01 00:06:18 -0700

    more license stuff

--- HappyGB/Xna/GbGame.cs
+++ HappyGB/Xna/GbGame.cs
@@ -1,9 +1,3 @@
-// /* 
-//  * Copyright © 2013 Corey Eckelman <corey@coreyeckelman.com>
-//  * This work is free. You can redistribute it and/or modify it under the
-//  * terms of the Do What The Fuck You Want To Public License, Version 2,
-//  * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details. 
-//  */
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
