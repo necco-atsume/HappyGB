ffd949824d9d088ded1095f56c3ece8ec007556c
Corey Eckelman <corey@coreyeckelman.com>
2016-07-04 19:57:57 -0700

    #2 Fix for failing build (missing gb file)

---
+++ HappyGBTests/TestCategories.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyGBTests
+{
+    internal class TestCategories
+    {
+        public static readonly string Unit = "Unit";
+        public static readonly string Regression = "Regression";
+        public static readonly string Integration = "Integration";
+    }
+}
